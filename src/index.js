c8d0baada36e25fa60225639c306b8257219670d
thefactmachine <mark.hatcher@thefactmachine>
2019-03-08 14:28:39 +1100

    05_hot

--- src/index.js
+++ src/index.js
@@ -5,11 +5,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-ReactDOM.render(<h1>Hey, there</h1>, document.getElementById('another_id'));
+ReactDOM.render(<h1>Hey, there mark</h1>, document.getElementById('another_id'));
 
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+if (module.hot) {
+	module.hot.accept();
+}
