b94fc0bf9bcf211d651af310c06c66e42aa85bd3
thefactmachine <mark.hatcher@thefactmachine>
2019-03-09 18:19:23 +1100

    splitting up the components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 const list = [
@@ -10,59 +9,84 @@ const list = [
     num_comments: 3,
     points: 4,
     objectID: 0,
-}, {
+  },
+  {
     title: 'Redux',
     url: 'https://redux.js.org/',
-    author: 'Mark  Hatcher, Andrew Clark',
+    author: 'Dan Abramov, Andrew Clark',
     num_comments: 2,
     points: 5,
     objectID: 1,
-}, ];
+  },
+];
 
-
-
-function isSearched(searchTerm) {
-  return function (item) {
-    return item.title.toLowerCase().indexOf(searchTerm.toLowerCase()) !== -1;
-  }
-}
-
+const isSearched = searchTerm => item =>
+  item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
 class App extends Component {
   constructor(props) {
     super(props);
-    
+
     this.state = {
-      list: list,
+      list,
       searchTerm: '',
     };
-    
+
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
-
-  } // constructor
+  }
 
   onSearchChange(event) {
-     this.setState({searchTerm: event.target.value});
-
+    this.setState({ searchTerm: event.target.value });
   }
 
   onDismiss(id) {
     const isNotId = item => item.objectID !== id;
-    const updatedList = this.state.list.filter(isNotId); 
+    const updatedList = this.state.list.filter(isNotId);
     this.setState({ list: updatedList });
-  } 
-
+  }
 
   render() {
-      const {searchTerm, list} = this.state;
-      return (
-        <div className="App">
-        <form>
-          <input type="text" value={searchTerm} onChange={this.onSearchChange} />
-        </form>      
-        {list.filter(isSearched(searchTerm)).map(item =>
+    const { searchTerm, list } = this.state;
+    return (
+      <div className="App">
+        <Search
+          value={searchTerm}
+          onChange={this.onSearchChange}
+        />
+        <Table
+          list={list}
+          pattern={searchTerm}
+          onDismiss={this.onDismiss}
+        />
+      </div>
+    );
+  }
+}
+
+class Search extends Component {
+  render() {
+    const { value, onChange } = this.props;
+    return (
+      <form>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+        />
+      </form>
+    );
+  }
+}
+
+class Table extends Component {
+  render() {
+    const { list, pattern, onDismiss } = this.props;
+    return (
+      <div>
+        {list.filter(isSearched(pattern)).map(item =>
           <div key={item.objectID}>
+          <h3>fuck this</h3>
             <span>
               <a href={item.url}>{item.title}</a>
             </span>
@@ -70,14 +94,17 @@ class App extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button onClick={() => this.onDismiss(item.objectID)} type="button"> 
+              <button
+                onClick={() => onDismiss(item.objectID)}
+                type="button"
+              >
                 Dismiss
               </button>
             </span>
           </div>
         )}
-        </div>
-      );
+      </div>
+    );
   }
 }
 
