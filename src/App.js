2d55369045f899b1cc8094d8ec6e5e3949073eab
thefactmachine <mark.hatcher@thefactmachine>
2019-03-09 19:43:59 +1100

    button

--- src/App.js
+++ src/App.js
@@ -92,9 +92,9 @@ class Table extends Component {
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
             <span>
-              <button onClick={() => onDismiss(item.objectID)} type="button" >
+              <Button onClick={() => onDismiss(item.objectID)}>
                 Dismiss
-              </button>
+              </Button>
             </span>
           </div>
         )}
@@ -102,5 +102,26 @@ class Table extends Component {
     );
   }
 }
+
+
+class Button extends Component {
+  render() {
+    const {onClick, className = '', children, } = this.props;
+
+    return (
+      <button onClick={onClick} className={className} type="button" >
+        {children}
+      </button>
+    );
+  }
+}
+
+
+
+
+
+
+
+
 
 export default App;
