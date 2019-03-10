b635f92aa7e8d4671ff83f14b1658cb6a14f08c4
thefactmachine <mark.hatcher@thefactmachine>
2019-03-10 16:36:07 +1100

    added style

--- src/App.js
+++ src/App.js
@@ -20,6 +20,18 @@ const list = [
   },
 ];
 
+const largeColumn = {
+  width: '40%',
+};
+
+const midColumn = {
+  width: '30%',
+};
+
+const smallColumn = {
+  width: '10%',
+};
+
 const isSearched = searchTerm => item =>
   item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
@@ -52,13 +64,15 @@ class App extends Component {
   render() {
     const { searchTerm, list } = this.state;
     return (
-      <div className="App">
-        <Search value={searchTerm} onChange={this.onSearchChange} Search />
-        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+      <div className="page">
+        <div className="interactions">
+          <Search value={searchTerm} onChange={this.onSearchChange} Search />
+        </div>
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />       
       </div>
     );
   }
-}
+} // Component
 
 // ==================================================================
 // ==================================================================
@@ -73,23 +87,28 @@ const Search = ({ value, onChange, children }) =>
 // ==================================================================
 
 const Table = ({list, pattern, onDismiss }) =>
-  <div>
+  <div className = "table">
     {list.filter(isSearched(pattern)).map(item =>
-      <div key={item.objectID}>
-        <span>
+      <div key={item.objectID} className="table-row">
+        
+        <span style={largeColumn}>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
-          <Button onClick={() => onDismiss(item.objectID)}>
+        <span style={midColumn}>{item.author}</span>
+        <span style={smallColumn}>{item.num_comments}</span>
+        <span style={smallColumn}>{item.points}</span>
+        
+        <span style={smallColumn}>
+          <Button onClick={() => onDismiss(item.objectID)} className="button-inline">
             Dismiss
           </Button>
         </span>
       </div>
     )}
   </div>
+
+ // ==================================================================
+// ==================================================================
 
 const Button = ({onClick, className = '', children, }) =>
   <button onClick={onClick} className={className} type="button" >
